1215b9ad8fcee3323f9810b205e0f26ad880bbc8
João Catré <joaomcatre@hotmail.com>
2023-12-11 05:45:24 +0000

    more layout

--- app/src/main/java/com/example/cm3/HomeFragment.java
+++ app/src/main/java/com/example/cm3/HomeFragment.java
@@ -7,10 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
 }
