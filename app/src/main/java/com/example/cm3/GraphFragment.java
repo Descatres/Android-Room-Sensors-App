59daef66e0edcf13f3f42e805f58e79d7792d3d6
João Catré <joaomcatre@hotmail.com>
2023-12-16 19:33:08 +0000

    TODO guardar currenttimemillis no mainactivity e processar a partir daí

--- app/src/main/java/com/example/cm3/GraphFragment.java
+++ app/src/main/java/com/example/cm3/GraphFragment.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -21,12 +24,17 @@ import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class GraphFragment extends Fragment {
 
     private LineChart lineChart;
     private FirebaseFirestore firestore;
+
+    private Spinner spinnerDataType;
 
     private ArrayList<Entry> temperatureEntries = new ArrayList<>();
     private ArrayList<Entry> humidityEntries = new ArrayList<>();
@@ -42,6 +50,23 @@ public class GraphFragment extends Fragment {
 
         firestore = FirebaseFirestore.getInstance();
         retrieveFirestoreData();
+
+        spinnerDataType = view.findViewById(R.id.spinner_data_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.data_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDataType.setAdapter(adapter);
+        spinnerDataType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                // Handle item selection
+                filterData(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing here
+            }
+        });
 
         return view;
     }
@@ -75,26 +100,51 @@ public class GraphFragment extends Fragment {
         lineChart.invalidate(); // Refresh the chart
     }
 
-    private void setData() {
-        LineDataSet temperatureDataSet = new LineDataSet(temperatureEntries, "Temperature");
-        temperatureDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        temperatureDataSet.setColor(Color.RED);
-        temperatureDataSet.setDrawCircles(true);
-        temperatureDataSet.setCircleColor(Color.RED);
+    private void filterData(int position) {
+        switch (position) {
+            case 0: // All
+                setData(temperatureEntries, temperatureTimeStamps, humidityEntries, humidityTimeStamps, "Temperature", "Humidity");
+                break;
+            case 1: // Temperature
+                setData(temperatureEntries, temperatureTimeStamps, null, null, "Temperature", null);
+                break;
+            case 2: // Humidity
+                setData(humidityEntries, humidityTimeStamps, null, null, null, "Humidity");
+                break;
+        }
+    }
 
-        LineDataSet humidityDataSet = new LineDataSet(humidityEntries, "Humidity");
-        humidityDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        humidityDataSet.setColor(Color.BLUE);
-        humidityDataSet.setDrawCircles(true);
-        humidityDataSet.setCircleColor(Color.BLUE);
+    private void setData(
+            ArrayList<Entry> dataSet1,
+            ArrayList<Long> timeStamps1,
+            ArrayList<Entry> dataSet2,
+            ArrayList<Long> timeStamps2,
+            String label1,
+            String label2
+    ) {
+        LineDataSet dataSetTemperature = new LineDataSet(dataSet1, label1);
+        dataSetTemperature.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSetTemperature.setColor(Color.RED);
+        dataSetTemperature.setDrawCircles(true);
+        dataSetTemperature.setCircleColor(Color.RED);
 
-        LineData lineData = new LineData(temperatureDataSet, humidityDataSet);
+        LineDataSet dataSetHumidity = new LineDataSet(dataSet2, label2);
+        dataSetHumidity.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSetHumidity.setColor(Color.BLUE);
+        dataSetHumidity.setDrawCircles(true);
+        dataSetHumidity.setCircleColor(Color.BLUE);
+
+        LineData lineData = new LineData(dataSetTemperature);
+
+        if (dataSet2 != null && timeStamps2 != null && label2 != null) {
+            dataSetTemperature.setLabel(label1);
+            dataSetHumidity.setLabel(label2);
+            lineData = new LineData(dataSetTemperature, dataSetHumidity);
+        }
 
         XAxis xAxis = lineChart.getXAxis();
-        String[] temperatureTimeStampsArray = getFormattedTimeStamps(temperatureTimeStamps);
-        String[] humidityTimeStampsArray = getFormattedTimeStamps(humidityTimeStamps);
-
-        xAxis.setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(temperatureTimeStampsArray));
+        String[] timeStampsArray = getFormattedTimeStamps(timeStamps1); // You can modify this to handle combined timestamps if needed
+        xAxis.setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(timeStampsArray));
 
         lineChart.setData(lineData);
         lineChart.invalidate(); // Refresh the chart
@@ -103,11 +153,15 @@ public class GraphFragment extends Fragment {
     private String[] getFormattedTimeStamps(ArrayList<Long> timeStamps) {
         String[] formattedTimeStamps = new String[timeStamps.size()];
         for (int i = 0; i < timeStamps.size(); i++) {
-            // Format the timestamp as needed
-            // For example, you can use SimpleDateFormat or convert to a readable format
-            formattedTimeStamps[i] = String.valueOf(timeStamps.get(i));
+            formattedTimeStamps[i] = formatDate(timeStamps.get(i));
         }
         return formattedTimeStamps;
+    }
+
+    private String formatDate(long timeStamp) {
+        // Format the timestamp as dd/MM/yy@HH:mm:ss
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy@HH:mm:ss", Locale.getDefault());
+        return sdf.format(new Date(timeStamp));
     }
 
     private void retrieveFirestoreData() {
@@ -127,33 +181,34 @@ public class GraphFragment extends Fragment {
                             QueryDocumentSnapshot document = dc.getDocument();
                             if (document.contains("temperature")) {
                                 Double temperature = document.getDouble("temperature");
+                                // TODO guardar currenttimemillis no mainactivity e processar a partir daí
                                 Long timeStamp = document.getTimestamp("t_stamp").getSeconds() * 1000; // Convert to milliseconds
 
                                 // Check if temperature value is not null
-                                if (temperature != null) {
+                                if (temperature != null && timeStamp != null) {
                                     temperatureEntries.add(new Entry(temperatureEntries.size() + 1, temperature.floatValue()));
                                     temperatureTimeStamps.add(timeStamp);
                                 } else {
-                                    // Handle the case where temperature is null
                                     Log.e("GraphFragment", "Temperature is null");
                                 }
                             }
 
                             if (document.contains("humidity")) {
                                 Double humidity = document.getDouble("humidity");
+                                // TODO guardar currenttimemillis no mainactivity e processar a partir daí
                                 Long timeStamp = document.getTimestamp("t_stamp").getSeconds() * 1000; // Convert to milliseconds
 
                                 // Check if humidity value is not null
-                                if (humidity != null) {
+                                if (humidity != null && timeStamp != null) {
                                     humidityEntries.add(new Entry(humidityEntries.size() + 1, humidity.floatValue()));
                                     humidityTimeStamps.add(timeStamp);
                                 } else {
-                                    // Handle the case where humidity is null
                                     Log.e("GraphFragment", "Humidity is null");
                                 }
                             }
                         }
-                        setData();
+//                        setData();
+                        filterData(spinnerDataType.getSelectedItemPosition());
                     }
                 });
     }
